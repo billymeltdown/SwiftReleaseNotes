317b090515d03885b4ab814f027efa7df6646873
William Gray <wgray@zetetic.net>
2021-03-03 11:17:26 -0500

    Initial Commit

---
+++ SwiftReleaseNotes/SwiftReleaseNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftReleaseNotesApp.swift
+//  SwiftReleaseNotes
+//
+//  Created by William Gray on 3/3/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftReleaseNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
